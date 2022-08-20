8f2e58a7091c2df4b1675ece9ad8c60d43cc460b
masahiro8 <masahiro819@gmail.com>
2022-08-20 11:18:37 +0900

    動画ローディング

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,6 +6,7 @@
         @loadeddata="onLoadedVideo"
         :src="src"
         playsinline
+        autoplay
         muted
         type="video/mp4"
       ></video>
@@ -20,9 +21,10 @@
 </template>
 
 <script>
+  import { fileLoader } from '../util/fileLoader';
   let vd = null;
   const url =
-    'https://storage.cloud.google.com/bkm_tmp/train_simulator/video/train.mp4?authuser=2&_ga=2.231403293.-326100595.1657302146&_gac=1.213460582.1657302146.CjwKCAjwq5-WBhB7EiwAl-HEkj3CeXr2A_isEI2c0_xz7JGg7LRI7wwxo2ot8eiYz84cxgavogXobBoCOxMQAvD_BwE';
+    'https://storage.googleapis.com/bkm_tmp/train_simulator/video/train.mp4';
   export default {
     name: 'HelloWorld',
     props: {
@@ -35,33 +37,44 @@
       };
     },
     mounted() {
+      this.loadData();
       this.elem = document.getElementById('trainVideo');
-
-      this.$nextTick(() => {
-        this.elem.addEventListener('canplaythrough', () => {
-          console.log(
-            'バッファリングを止めることなく、' +
-              '動画全体を再生できると思います。'
-          );
-        });
-        this.elem.addEventListener('canplay', () => {
-          console.log(
-            '動画は開始できますが、最後まで再生されるかどうかはわかりません。'
-          );
-        });
-        this.elem.addEventListener('seeking', () => {
-          console.log('動画は新しい位置をシーク中です。');
-        });
-        this.elem.addEventListener('seeked', () => {
-          console.log('動画が探していた再生位置を見つけました。');
-        });
-        this.elem.addEventListener('ratechange', () => {
-          console.log('再生レートが変わりました。');
-        });
-        this.src = url;
-      });
     },
     methods: {
+      async loadData() {
+        const result = await fileLoader().getFiles([{ url }], (progress) => {
+          const total = Object.keys(progress).length * 100;
+          const current = Object.keys(progress)
+            .map((key) => progress[key].progress)
+            .reduce((prev, current) => prev + current);
+          const loadingProgress = (Math.ceil(current) / total) * 100;
+          console.log('■ ', loadingProgress);
+        });
+        console.log('result', result);
+        this.$nextTick(() => {
+          this.elem.addEventListener('canplaythrough', () => {
+            console.log(
+              'バッファリングを止めることなく、' +
+                '動画全体を再生できると思います。'
+            );
+          });
+          this.elem.addEventListener('canplay', () => {
+            console.log(
+              '動画は開始できますが、最後まで再生されるかどうかはわかりません。'
+            );
+          });
+          this.elem.addEventListener('seeking', () => {
+            console.log('動画は新しい位置をシーク中です。');
+          });
+          this.elem.addEventListener('seeked', () => {
+            console.log('動画が探していた再生位置を見つけました。');
+          });
+          this.elem.addEventListener('ratechange', () => {
+            console.log('再生レートが変わりました。');
+          });
+          this.src = url;
+        });
+      },
       play() {
         vd.play();
       },
